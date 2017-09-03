70fdf2172f42d5d1a1b5a68de27455d00cb3c926
kivahb <31216713+kivahb@users.noreply.github.com>
2017-09-03 13:53:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(createtemplate(articleOne));
+    res.send(createtemplate(articleOne));
 });  
 app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
