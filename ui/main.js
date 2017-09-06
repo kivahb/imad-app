84995ecaae693544364fc292761754b7e8705332
kivahb <31216713+kivahb@users.noreply.github.com>
2017-09-06 11:16:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,6 @@ element.innerHTML =' New value';
 // move the image
 var img = document.getElementById('madi');
 img.onclick = function()  {
-  img.style.marginLeft = '100px';
+  madi
+  madi.style.marginLeft = '100px';
     };
