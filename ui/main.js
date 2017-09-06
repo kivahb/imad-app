5a5e137637488eb1b845bc5790a38c2296deb467
kivahb <31216713+kivahb@users.noreply.github.com>
2017-09-06 11:13:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ var element = document.getById(',main-text'
     );
     
 element.innerHTML =' New value';
+
+// move the image
+var img = document.getElementById('img');
+img.onclick = function()  {
+  img.style.marginLeft = '100px';
+    };
