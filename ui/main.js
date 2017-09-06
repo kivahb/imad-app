f1da02c576dfc9660aa8b2ea4a54a12fd4e4e402
kivahb <31216713+kivahb@users.noreply.github.com>
2017-09-06 11:20:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById(',main-text'
     );
     
-element.innerHTML =' New value';
+element.innerHTML = 'New value';
 
 // move the image
 var img = document.getElementById('madi');
